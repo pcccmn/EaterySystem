0046f549193302bbb98979a7bd95a44febf6efb2
pcccmn <mlmsamliew@gmail.com>
2022-05-14 22:23:20 +0800

    initialized springboot demo

---
+++ CustomerOrderSystem-SpringBoot/src/main/java/main/CustomerOrderSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrderSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerOrderSystemSpringBootApplication.class, args);
+	}
+
+}
